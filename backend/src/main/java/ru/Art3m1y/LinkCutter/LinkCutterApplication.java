56f89dab8be1bb1c52bce760191ccb5bd320d53c
Artem Kuryshkin <70939390+Art3m1y@users.noreply.github.com>
2023-03-17 13:12:49 +0300

    added backend folder

---
+++ backend/src/main/java/ru/Art3m1y/LinkCutter/LinkCutterApplication.java
@@ -0,0 +1,13 @@
+package ru.Art3m1y.LinkCutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkCutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkCutterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/Art3m1y/LinkCutter/LinkCutterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Art3m1y.LinkCutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkCutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
