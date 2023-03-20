befa4c2c3a14eddbac182e56c8e890daedb8c2ac
Artem Kuryshkin <70939390+Art3m1y@users.noreply.github.com>
2023-03-20 22:32:53 +0300

    Edited gradle file

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("com.diffplug.eclipse.apt") version "3.26.0"
 }
 
+val mapstructVersion = "1.5.3.Final"
 group = "ru.Art3m1y"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -23,13 +24,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.mapstruct:mapstruct:1.5.3.Final")
+	implementation("org.mapstruct:mapstruct:${mapstructVersion}")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:${mapstructVersion}")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testAnnotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+	testAnnotationProcessor("org.mapstruct:mapstruct-processor:${mapstructVersion}")
 }
 
 tasks.withType<Test> {
